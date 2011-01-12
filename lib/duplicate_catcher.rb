c66f0857e3007a5d7c556a1b87f43d677e635dc0
John Metta <john@johnmetta.com>
2011-01-11 16:57:41 -0800

    meh

--- lib/duplicate_catcher.rb
+++ lib/duplicate_catcher.rb
@@ -7,7 +7,7 @@ module DuplicateCatcher
   module ClassMethods
     def add_word args
       lst = find_by_word(args[:word])
-      if lst
+      if lst.length > 0
         return
       else 
         create(args)
@@ -16,7 +16,7 @@ module DuplicateCatcher
 
     def add_name args
       lst = find_by_name(args[:name])
-      if lst
+      if lst.length > 0
         return
       else 
         create(args)
